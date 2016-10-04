0c417860608f1f9f3c883bec2a04a44142614882
xiaoye00 <120778530@qq.com>
2016-10-04 18:43:35 +0800

    add py2

---
+++ py2.py
@@ -0,0 +1,23 @@
+#division
+ret = 1/2
+print ret
+#exact division
+ret = 1//2
+print ret
+ret = 1.0//2.0
+print ret
+#
+ret = 9%3
+print ret
+ret = 10%3
+print ret
+#
+ret = 2**3
+print ret
+ret = -2**2
+print ret
+ret = (-2)**2
+print ret
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
-10.32
 these are the files that I learn the python language.
+
+py1  hello world ,config the run envariment on visual studio code
+
+py2   learn the symoble of mathmatic
 
