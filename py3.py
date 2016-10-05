6b9049349fdd445f61bae1476ebe068dac3268ec
xiaoye00 <120778530@qq.com>
2016-10-05 18:36:33 +0800

    push py3

---
+++ py3.py
@@ -0,0 +1,29 @@
+
+#list
+
+edward = ['Edwrd Gumby',42]
+
+print edward[0]
+
+#itrable list 
+
+john = ['John Smith',50]
+
+database = [edward,john]
+
+print database
+
+#index
+
+greeting = 'Hello'
+
+#seek it since the end
+
+print greeting[-1]
+
+#begin one is index 0
+
+print greeting[0]
+
+
+
