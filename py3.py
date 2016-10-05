433da8d96e49475144817ccae691f9aa362ce96d
xiaoye00 <120778530@qq.com>
2016-10-05 19:36:56 +0800

    add some examples in py3.py

--- py3.py
+++ py3.py
@@ -21,9 +21,43 @@ greeting = 'Hello'
 
 print greeting[-1]
 
+print greeting[0:1]
+
 #begin one is index 0
 
 print greeting[0]
+
+tag = '<a href = "http://www.python.org">Python wed site</a>'
+
+print tag[9:30]
+
+print tag[32:-4]
+
+numbers = [1,2,3,4,5,6,7,8,9,10];
+
+print numbers [7:10]
+
+print numbers[-3:-1]
+
+print numbers[-3:]
+
+
+
+print numbers[0:10:1]
+
+print numbers[0:10:2]
+
+print numbers[10:0:-1]
+
+print numbers[10::-1]
+
+numbers = [1,2,3]+[4,5,6]
+
+print numbers
+
+numbers = numbers*2
+
+print numbers
 
 
 
--- readme.txt
+++ readme.txt
@@ -4,3 +4,7 @@ py1  hello world ,config the run envariment on visual studio code
 
 py2   learn the symoble of mathmatic
 
+py3   learn the list step1 ,about the base operation
+
+
+
