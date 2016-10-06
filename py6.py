ff145f56450aa40a80326939d5d0380a013d04e2
xiaoye00 <120778530@qq.com>
2016-10-06 13:16:28 +0800

    add list function over

--- py6.py
+++ py6.py
@@ -19,5 +19,51 @@ kiights = ['We','are','the','knights','who','say','ni']
 
 print kiights.index('who')
 
+#insert list
+
+numbers = [1,2,3,4,5]
+
+numbers.insert(3,'four')
+
+print numbers
+
+numbers.pop()
+
+print numbers
+
+numbers.pop(0)
+
+print numbers
+
+x = ['to','be','or','nor','to','be']
+
+x.remove('be')
+
+print x
+
+x.reverse()
+
+print x
+
+x = [1,5,3,2,6,9,4]
+
+#if  need save the origin list , should do like this y = x[:]
+#if do like this  y = x. it dose not effct ,because they point the same place
+
+y = x[:]
+
+y.sort()
+
+print x
+print y
+
+#another two parameter key and reserve 
+
+
+
+
+
+
+
 
 
